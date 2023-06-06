27db25dd6a7da4c326401cb8250a36a5c17e7a1d
すえ <souhait.0614@gmail.com>
2023-06-06 10:34:37 +0900

    テストの記述を変更

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest"
+import { describe, expect, it, test } from "vitest"
 
 import {
   Converter,
@@ -26,131 +26,190 @@ const error = () => {
   throw dummyError
 }
 
-describe("Converter", () => {
-  it.concurrent("Normal Convert", async () => {
-    const plugins = {
-      upc: new Plugin({ convertFunction: [upperCase] }),
+test.concurrent("Basic Usage", async () => {
+  const converter = new Converter({
+    plugins: {
+      upperCase: new Plugin({ convertFunction: [upperCase] }),
       x4: new Plugin({ convertFunction: [x4] }),
-      amg: new Plugin({ convertFunction: [amazingProcessing] }),
-    } as const satisfies Plugins<any>
-    const converter = new Converter({
-      plugins,
-    })
+      amazingProcessing: new Plugin({ convertFunction: [amazingProcessing] }),
+    } as const,
+  })
 
-    expect(converter.plugins).toEqual(plugins)
+  const input = "subway"
 
-    const input = "subway"
-    const expectedOutput = "9"
+  const [output, details] = await converter.convert(input, [
+    { id: "upperCase" },
+    {
+      id: "x4",
+      option: {
+        target: "A",
+      },
+    },
+    { id: "amazingProcessing" },
+  ] as const)
 
-    const options = [
-      { id: "upc" },
-      {
-        id: "x4",
+  // test
+  const expectedOutput = "9"
+  const expectedDetails = [
+    {
+      id: "upperCase",
+      ok: true,
+      output: "SUBWAY",
+      args: {
+        input: "subway",
+      },
+      error: [],
+    },
+    {
+      id: "x4",
+      ok: true,
+      output: "SUBWAAAAY",
+      args: {
+        input: "SUBWAY",
         option: {
           target: "A",
         },
       },
-      { id: "amg" },
-    ] as const satisfies readonly ConvertOrder<typeof plugins>[]
-    const expectedDetails: ConvertResultDetails<typeof options> = [
-      {
-        id: "upc",
-        ok: true,
-        output: "SUBWAY",
-        args: {
-          input: "subway",
-        },
-        error: [],
+      error: [],
+    },
+    {
+      id: "amazingProcessing",
+      ok: true,
+      output: "9",
+      args: {
+        input: "SUBWAAAAY",
       },
-      {
-        id: "x4",
-        ok: true,
-        output: "SUBWAAAAY",
-        args: {
-          input: "SUBWAY",
-          option: {
-            target: "A",
-          },
-        },
-        error: [],
-      },
-      {
-        id: "amg",
-        ok: true,
-        output: "9",
-        args: {
-          input: "SUBWAAAAY",
-        },
-        error: [],
-      },
-    ]
+      error: [],
+    },
+  ]
 
-    const [output, details] = await converter.convert(input, options)
+  expect(output).toEqual(expectedOutput)
+  expect(details).toEqual(expectedDetails)
+})
 
-    expect(output).toEqual(expectedOutput)
-    expect(details).toEqual(expectedDetails)
+test.concurrent("Normal Convert", async () => {
+  const plugins = {
+    upc: new Plugin({ convertFunction: [upperCase] }),
+    x4: new Plugin({ convertFunction: [x4] }),
+    amg: new Plugin({ convertFunction: [amazingProcessing] }),
+  } as const satisfies Plugins<any>
+  const converter = new Converter({
+    plugins,
   })
 
-  it.concurrent("Fallback Convert", async () => {
-    const plugins = {
-      upc: new Plugin({ convertFunction: [error, upperCase] }),
-      x4: new Plugin({ convertFunction: [error] }),
-      amg: new Plugin({ convertFunction: [amazingProcessing, error] }),
-    } as const satisfies Plugins<any>
-    const converter = new Converter({
-      plugins,
-    })
+  expect(converter.plugins).toEqual(plugins)
 
-    expect(converter.plugins).toEqual(plugins)
+  const input = "subway"
+  const expectedOutput = "9"
 
-    const input = "subway"
-    const expectedOutput = "6"
-
-    const options = [
-      { id: "upc" },
-      {
-        id: "x4",
+  const options = [
+    { id: "upc" },
+    {
+      id: "x4",
+      option: {
+        target: "A",
+      },
+    },
+    { id: "amg" },
+  ] as const satisfies readonly ConvertOrder<typeof plugins>[]
+  const expectedDetails: ConvertResultDetails<typeof options> = [
+    {
+      id: "upc",
+      ok: true,
+      output: "SUBWAY",
+      args: {
+        input: "subway",
+      },
+      error: [],
+    },
+    {
+      id: "x4",
+      ok: true,
+      output: "SUBWAAAAY",
+      args: {
+        input: "SUBWAY",
         option: {
           target: "A",
         },
       },
-      { id: "amg" },
-    ] as const satisfies readonly ConvertOrder<typeof plugins>[]
-    const expectedDetails: ConvertResultDetails<typeof options> = [
-      {
-        id: "upc",
-        ok: true,
-        output: "SUBWAY",
-        args: {
-          input: "subway",
-        },
-        error: [dummyError],
+      error: [],
+    },
+    {
+      id: "amg",
+      ok: true,
+      output: "9",
+      args: {
+        input: "SUBWAAAAY",
       },
-      {
-        id: "x4",
-        ok: false,
-        args: {
-          input: "SUBWAY",
-          option: {
-            target: "A",
-          },
-        },
-        error: [dummyError],
-      },
-      {
-        id: "amg",
-        ok: true,
-        output: "6",
-        args: {
-          input: "SUBWAY",
-        },
-        error: [],
-      },
-    ]
+      error: [],
+    },
+  ]
 
-    const [output, details] = await converter.convert(input, options)
+  const [output, details] = await converter.convert(input, options)
 
-    expect(output).toEqual(expectedOutput)
-    expect(details).toEqual(expectedDetails)
+  expect(output).toEqual(expectedOutput)
+  expect(details).toEqual(expectedDetails)
+})
+
+test.concurrent("Fallback Convert", async () => {
+  const plugins = {
+    upc: new Plugin({ convertFunction: [error, upperCase] }),
+    x4: new Plugin({ convertFunction: [error] }),
+    amg: new Plugin({ convertFunction: [amazingProcessing, error] }),
+  } as const satisfies Plugins<any>
+  const converter = new Converter({
+    plugins,
   })
+
+  expect(converter.plugins).toEqual(plugins)
+
+  const input = "subway"
+  const expectedOutput = "6"
+
+  const options = [
+    { id: "upc" },
+    {
+      id: "x4",
+      option: {
+        target: "A",
+      },
+    },
+    { id: "amg" },
+  ] as const satisfies readonly ConvertOrder<typeof plugins>[]
+  const expectedDetails: ConvertResultDetails<typeof options> = [
+    {
+      id: "upc",
+      ok: true,
+      output: "SUBWAY",
+      args: {
+        input: "subway",
+      },
+      error: [dummyError],
+    },
+    {
+      id: "x4",
+      ok: false,
+      args: {
+        input: "SUBWAY",
+        option: {
+          target: "A",
+        },
+      },
+      error: [dummyError],
+    },
+    {
+      id: "amg",
+      ok: true,
+      output: "6",
+      args: {
+        input: "SUBWAY",
+      },
+      error: [],
+    },
+  ]
+
+  const [output, details] = await converter.convert(input, options)
+
+  expect(output).toEqual(expectedOutput)
+  expect(details).toEqual(expectedDetails)
 })
