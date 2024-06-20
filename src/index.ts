e67b68011033d4f6ebd9e0b1bea0907b5ca41044
すえ <souhait.0614@gmail.com>
2024-06-20 13:44:58 +0900

    indexからに移動

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
+/**
+ * @module
+ *
+ * 文字列を加工して返却する関数をプラグインとして設定し、指定した順序、オプションに従って文字列を変換します
+ *
+ * @example
+ * ```typescript
+ * import { Converter, type Plugin } from "@submarin-converter/core"
+ * const double: Plugin<undefined> = {
+ *   convertFunctions: [(text) => text + text],
+ * };
+ * const suffix: Plugin<{ suffix: string }> = {
+ *   defaultOption: { suffix: "" },
+ *   convertFunctions: [(text, option) => text + option.suffix],
+ * };
+ * const converter = new Converter({
+ *   double,
+ *   suffix,
+ * });
+ *
+ * const { text } = await converter.convert(
+ *   "Foo",
+ *   [
+ *     "double",
+ *     {
+ *       name: "suffix",
+ *       option: { suffix: "Bar" },
+ *     },
+ *   ] as const,
+ * );
+ *
+ * console.log(text) // "FooFooBar"
+ * ```
+ */
+
 export * from "./Converter.ts";
 export type * from "./types.ts";
