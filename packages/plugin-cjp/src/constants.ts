13c45cdd9ae84fbe67d8ad9a5335618c04d2fc84
すえ <souhait.0614@gmail.com>
2025-02-13 23:15:47 +0900

    metadata修正

--- packages/plugin-cjp/src/constants.ts
+++ packages/plugin-cjp/src/constants.ts
@@ -1,7 +1,9 @@
+import type { PluginMetaData } from "@submarin-converter/core";
+
 export const metaData = {
   displayName: "怪レい日本语",
   description: "日本語を怪レい日本语に変換します",
   homepage: "https://github.com/Submarinonline/cjp.js",
-  repository: "https://github.com/souhait0614/submarin-converter-plugin-cjp",
+  repository: "https://github.com/souhait0614/submarin-converter",
   author: "すえ",
-};
+} as const satisfies PluginMetaData;
--- packages/plugin-genhera/src/constants.ts
+++ packages/plugin-genhera/src/constants.ts
@@ -1,8 +1,9 @@
+import type { PluginMetaData } from "@submarin-converter/core";
+
 export const metaData = {
   displayName: "メンヘラ",
   description: "日本語をﾒﾝﾍﾗっぽぃ日本語に変換します",
   homepage: "https://github.com/Submarinonline/genhera.js",
-  repository:
-    "https://github.com/souhait0614/submarin-converter-plugin-genhera",
+  repository: "https://github.com/souhait0614/submarin-converter",
   author: "すえ",
-};
+} as const satisfies PluginMetaData;
